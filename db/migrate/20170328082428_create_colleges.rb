021817d48d8da1065cd475da7e2e3467134a74fa
Alex McBride <xandermcbride@gmail.com>
2017-03-28 09:26:05 +0100

    Added college migration

---
+++ db/migrate/20170328082428_create_colleges.rb
@@ -0,0 +1,12 @@
+class CreateColleges < ActiveRecord::Migration[5.0]
+  def change
+    create_table :colleges do |t|
+      t.string :name
+      t.text :description
+      t.string :address
+      t.string :postcode
+
+      t.timestamps
+    end
+  end
+end

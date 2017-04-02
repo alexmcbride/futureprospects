22453833ddc57f0df84fd968e032f3a748ebed04
Alex McBride <xandermcbride@gmail.com>
2017-04-02 15:10:42 +0100

    Lots of improvements to login views, also added login breadcrumbs.

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -32,3 +32,44 @@ crumb :college do |college|
   parent :colleges
   link college.name, college_path(college)
 end
+
+# students
+crumb :register do
+  parent :root
+  link 'Register', new_student_registration_path
+end
+
+crumb :sign_in do
+  parent :root
+  link 'Sign In', new_student_session_path
+end
+
+crumb :sign_in do
+  parent :root
+  link 'Sign In', new_student_session_path
+end
+
+crumb :profile do
+  parent :root
+  link 'Profile', edit_student_registration_path
+end
+
+crumb :forgot_password do
+  parent :root
+  link 'Recover Password', new_student_password_path
+end
+
+crumb :change_password do
+  parent :root
+  link 'Change Password', edit_student_password_path
+end
+
+crumb :resend_unlock do
+  parent :root
+  link 'Resend Unlock Instructions', new_student_unlock_path
+end
+
+crumb :resend_confirmation do
+  parent :root
+  link 'Resend Confirmation Instructions', new_student_confirmation_path
+end

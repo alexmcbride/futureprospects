36423bdc7d85b502b81b6f55f6468473e6b4192b
Alex McBride <xandermcbride@gmail.com>
2017-04-02 17:51:09 +0100

    Added missing file. :(

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,18 +3,43 @@ class Student < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :timeoutable
+         :confirmable, :lockable, :timeoutable, :authentication_keys => [:login]
 
+  # Validators
   validates :first_name, presence: true
   validates :family_name, presence: true
   validates :scottish_candidate_number, presence: true, uniqueness: true
   validates :national_insurance_number, presence: true, uniqueness: true
 
+  # Properties
+  attr_accessor :login
+
+  # ActiveRecord callbacks
   before_create :before_create
+
+  # Sets the login value.
+  def login=(login)
+    @login = login
+  end
+
+  # Gets login, username, or email, depending on which is set.
+  def login
+    @login || self.username || self.email
+  end
+
+  # Overrides Devise sign in to allow both username and email address to be used.
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 
   # Called before student is created, in order to generate username.
   def before_create
-    # Keep generating username until we find one that's not being used
+    # Keep generating a username until we find one that's not being used
     while true
       username = generate_username self.first_name, self.family_name
       if username_free? username
@@ -32,7 +57,7 @@ class Student < ApplicationRecord
     initial + family_name + number.to_s
   end
 
-  # Gets a boolean indicating if the username is inuse or not.
+  # Gets a boolean indicating if the username is in use or not.
   def username_free?(username)
     student = Student.find_by_username username
     student.nil?

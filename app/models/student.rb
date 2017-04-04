6a4271195717eb95f8711573a5e904eff56bfb9f
Alex McBride <xandermcbride@gmail.com>
2017-04-04 10:12:46 +0100

    Tweaks to SCN validation, added NIN regex validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,6 +9,7 @@ class Student < ApplicationRecord
   validates :scottish_candidate_number, presence: true, uniqueness: true, length: {minimum: 9, maximum: 9}, numericality: {only_integer: true}
   validate :validate_scn
   validates :national_insurance_number, presence: true, uniqueness: true, length: {maximum: 9}
+  validates_format_of :national_insurance_number, with: /^(?!BG)(?!GB)(?!NK)(?!KN)(?!TN)(?!NT)(?!ZZ)(?:[A-CEGHJ-PR-TW-Z][A-CEGHJ-NPR-TW-Z])(?:\s*\d\s*){6}([A-D]|\s)$/, multiline: true
 
   # Foreign Key
   has_many :applications
@@ -72,7 +73,7 @@ class Student < ApplicationRecord
   def generate_username(first_name, family_name)
     initial = first_name[0].downcase
     family_name = family_name.downcase
-    number = rand(100...999)
+    number = rand(100..999)
     initial + family_name + number.to_s
   end
 
@@ -83,14 +84,44 @@ class Student < ApplicationRecord
 
   # Checks if a scottish candidate number is correct: https://www.hesa.ac.uk/collection/c15051/a/scn
   def validate_scn
-    nums = self.scottish_candidate_number.chars.map { |c| c.to_i }
+    nums = scottish_candidate_number.chars.map { |c| c.to_i }
+    check_digit = Student.generate_check_digit nums
+    # As check is single digit a ten is invalid
+    if check_digit == 10 or check_digit != nums.last
+      errors.add(:scottish_candidate_number, 'did not pass checksum')
+    end
+  end
+
+  # Generates a valid scottish candidate number for testing
+  # First 2 digits are year
+  # Next 6 are randomly generated
+  # Last digit is check digit
+  def self.generate_scn
+    year = DateTime.now.year
+    tens = year % 1000 / 10
+    ones = year % 1000 % 10
+    # Keep generating until we get a check digit that doesn't end in 10
+    scn = ''
+    begin
+      randoms = (0...6).each.map {|i| rand(0..9) }
+      number = [tens, ones] + randoms
+      check_digit = Student.generate_check_digit(number)
+      scn = number.join + check_digit.to_s
+    end until check_digit != 10
+    scn
+  end
+
+  # Generates a valid check digit for a SCN.
+  # Multiply each char by weight and sum result
+  # Divide sum by 11 and store remainder
+  # If remainder is 0 then check is 0
+  # Otherwise subtract remainder from 11 to get check digit
+  def self.generate_check_digit(nums)
     sum = 0
     [3, 2, 7, 6, 5, 4, 3, 2].each.with_index do |v, i|
       sum += nums[i] * v
     end
-    check_digit = 11 - (sum % 11)
-    if check_digit == 1 or check_digit != nums.last
-      errors.add(:scottish_candidate_number, 'did not pass checksum')
-    end
+    remainder = sum % 11
+    remainder == 0 ? 0 : 11 - remainder
   end
 end

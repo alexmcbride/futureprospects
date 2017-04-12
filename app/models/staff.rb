3ea3a459ff1082eb555664950bc7dc636002792e
Alex McBride <xandermcbride@gmail.com>
2017-04-12 11:22:26 +0100

    Changed staff to make emails automatically confirmed when created

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,10 +1,15 @@
 class Staff < User
   rolify
 
-  # Validators
   validates :job_title, presence: true, length: {maximum: 35}
   validates :college_id, presence: true
 
-  # Foreign keys
   belongs_to :college
+  before_create :before_staff_create
+
+  # Automatically confirm staff email.
+  def before_staff_create
+    self.confirmed_at = DateTime.now
+  end
+
 end

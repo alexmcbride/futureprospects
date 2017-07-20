9ae2bda212b434e2583542018880ce94fc8df715
Alex McBride <xandermcbride@gmail.com>
2017-07-20 19:23:05 +0100

    Added skip_jwt to google_oauth config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,7 +252,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH2_CLIENT_ID'], ENV['GOOGLE_OAUTH2_CLIENT_SECRET']
+  config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH2_CLIENT_ID'], ENV['GOOGLE_OAUTH2_CLIENT_SECRET'], skip_jwt: true
   config.omniauth :facebook, ENV['FACEBOOK_OAUTH_APP_ID'], ENV['FACEBOOK_OAUTH_APP_SECRET'], scope: 'email,public_profile', info_fields: 'email,first_name,last_name'
   config.omniauth :twitter, ENV['TWITTER_OAUTH_APP_ID'], ENV['TWITTER_OAUTH_APP_SECRET']#, scope: 'email,public_profile', info_fields: 'email,first_name,last_name'
 
